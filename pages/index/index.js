7544bb6e6cb8366046350782217b048eb728329e
齐林河 <1277859500@qq.com>
2019-07-30 20:39:33 +0800

    首页-轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 轮播图数据
+    swiperList: []
 
   },
 
@@ -12,13 +14,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getSwiperList()
+  },
+  // 获取轮播图数据
+  getSwiperList() {
     wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
       success: (result) => {
         console.log(result);
+        this.setData({
+          swiperList: result.data.message
+        })
       }
     });
-
   },
 
   /**
