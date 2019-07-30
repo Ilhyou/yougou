e7aca3968f8b04dd4221e5a23c71dd900232aa69
齐林河 <1277859500@qq.com>
2019-07-30 23:02:28 +0800

    分类页面-初次动态渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,18 +1,45 @@
-// pages/category/index.js
+import {
+  request
+} from "../../request/index.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧的菜单数组
+    leftMenuList: [],
+    // 右侧 商品内容数组  
+    rightGoodsList: [],
+    // 选中的菜单
+    currentIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getCategoryList()
+  },
+  // 获取分类数据
+  getCategoryList() {
+    request({
+      url: "/categories"
+    }).then(res => {
+      console.log(res);
+      let leftMenuList = res.map((v, i) => ({
+        cat_name: v.cat_name,
+        cat_id: v.cat_id
+      }));
+      // 这个是大家电对象 里面的children 数组
+      let rightGoodsList = res[0].children;
+      this.setData({
 
+        leftMenuList,
+        rightGoodsList
+
+      })
+    })
   },
 
   /**
