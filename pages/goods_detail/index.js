823a1031f4b0ea12911542552b01755687a8feb2
齐林河 <1277859500@qq.com>
2019-08-01 19:01:27 +0800

    商品详情-动态渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,18 +1,46 @@
 // pages/goods_detail/index.js
+import {
+  request
+} from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options);
+    let msg = "111-222-333-222";
+    let newMsg = msg.replace('222', 'aaa');
 
+    // let newMsg=msg.replace(/222/g,'aaa');
+    console.log(newMsg);
+    this.getGoodsDetail(options.goods_id);
+  },
+  // 获取商品的详情数据
+  async getGoodsDetail(goods_id) {
+    const res = await request({
+      url: "/goods/detail",
+      data: {
+        goods_id
+      }
+    });
+    this.setData({
+      // 只存放要用到的数据
+      goodsObj: {
+        goods_name: res.goods_name,
+        goods_price: res.goods_price,
+        goods_introduce: res.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: res.pics
+      }
+    })
   },
 
   /**
