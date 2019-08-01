3317d8f0cae2a9f363027d3a546febdc0d0c11ce
齐林河 <1277859500@qq.com>
2019-08-01 15:25:41 +0800

    分类-实现缓存效果

--- pages/category/index.js
+++ pages/category/index.js
@@ -12,6 +12,15 @@ import {
   1 以前的dom 可以直接操作 滚动条的属性 dom.scrollTop=0 
   2 scroll-view标签的 scrollTop 属性
   3 右侧内容切换的时候 再手动给它赋值即可 
+4 使用小程序的本地存储技术（h5的本地存储 localStorage） 来添加缓存效果 
+  0 复习一下 h5的本地存储的技术 
+  1 web中的本地存储和小程序中的本地存储的使用区别 
+  2 实现缓存的需求
+    1 发送请求之前 先判断有没有缓存数据  
+      1 假设存入存储中的对象 key='cates'   {time:Date.now(),data:接口的返回值 []}
+    2 没有缓存数据 直接发送新请求 获取数据 同时吧新的输入存入到本地存储中  
+    3 有缓存数据 并且数据 没有过期 我们自己定一个过期时间 ！！！！
+      此时再使用 缓存数据 
  */
 Page({
 
@@ -26,18 +35,45 @@ Page({
     // 选中的菜单
     currentIndex: 0,
     // 右侧滚动条的距离
-    scrollTop:0
+    scrollTop: 0
   },
   // 接口的返回值
   // 如果 这些数据不需要在页面中渲染，那么就没有必要放到data中
   // 因为 小程序中会对data的数据 进行传输 传输到视图层也就是 wxml中 
-  Cates:[],
+  Cates: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCategoryList()
+    // 1 发送请求之前 先判断 本地存储中有没有旧的数据
+    // 默认值 空字符串 null  bool  都是false 
+    let cates = wx.getStorage('cates');
+    // 没有数据 
+    if (!cates) {
+      this.getCategoryList()
+    } else {
+      // 有数据  判断一下时间是否过期！！！ 
+      // Date.now() 单位是毫秒   暂时定20s的过期时间！！！
+      if (Date.now() - cates.time >= 20 * 1000) {
+        // 获取过期  重新发送请求来获取 
+        this.getCategoryList();
+      } else {
+        // 数据没有过期 可以直接使用
+        this.Cates = cates.data;
+        let leftMenuList = this.Cates.map((v, i) => ({
+          cat_name: v.cat_name,
+          cat_id: v.cat_id
+        }));
+        // 这个是大家电对象 里面的children 数组
+        let rightGoodsList = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightGoodsList
+
+        })
+      }
+    }
   },
   // 获取分类数据
   getCategoryList() {
@@ -46,7 +82,13 @@ Page({
     }).then(res => {
       console.log(res);
       // 给全局参数 赋值
-      this.Cates=res;
+      this.Cates = res;
+      // 把接口的数据存入到本地存储中 
+      wx.setStorage("cates", {
+        time: Date.now(),
+        data: this.Cates
+      });
+
       // let leftMenuList = res.map((v, i) => {
       //   return {
       //     cat_name: v.cat_name,
@@ -73,11 +115,11 @@ Page({
       index
     } = e.currentTarget.dataset;
     // 实现菜单的激活选中
-    let rightGoodsList =  this.Cates[index].children;
+    let rightGoodsList = this.Cates[index].children;
     this.setData({
       currentIndex: index,
       rightGoodsList,
-      scrollTop:0
+      scrollTop: 0
     })
   },
 
