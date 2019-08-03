047e0dea2d5955e1d52288169d7b1a7d2384b24f
齐林河 <1277859500@qq.com>
2019-08-03 17:44:19 +0800

    购物车收货地址-es7的async

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -15,6 +15,12 @@
     1 诱导用户 打开 授权页面( wx.openSetting)。等用户重新给与权限之后
     2 再调用获取收货地址 
  */
+import regeneratorRuntime from '../../lib/runtime/runtime';
+import {
+  getSetting,
+  openSetting,
+  chooseAddress
+} from "../../utils/asyncWx";
 Page({
 
   /**
@@ -24,41 +30,55 @@ Page({
 
   },
   // 获取收货地址
-  handleChooseAddress() {
-    // 1 获取用户对小程序的授权信息
-    wx.getSetting({
-      success: (result1) => {
-        // 获取到了授权信息
-        console.log(result1);
-        const scopeAddress = result1.authSetting['scope.address'];
-        // 用户授权过  或者 用户 从来没有调用过收货地址  
-        if (scopeAddress === true || scopeAddress === undefined) {
-          // 1.1 调用收货地址
-          wx.chooseAddress({
-            success: (result2) => {
-              console.log(result2);
-            }
-          });
-        } else {
-          // 2.1用户 点击 拒绝收货地址  诱导用户 打开授权页面 再调用获取收货地址
-          wx.openSetting({
-            success: (result) => {
-              // 2.2 再去调用 获取收货地址
-              wx.chooseAddress({
-                success: (result3) => {
-                  console.log(result3);
-                },
-                fail: () => {},
-                complete: () => {}
-              });
-            }
-          });
+  async handleChooseAddress() {
+    // 1 获取授权信息
+    var res1 = getSetting();
+    const scopeAddress = res1.authSetting['scope.address'];
+    // 2 对授权信息判断
+    if (scopeAddress === true || scopeAddress === undefined) {
+      // 2.1直接调用获取收货地址的api
+      const res2 = await chooseAddress();
+      console.log(res2);
+    } else {
+      // 2.2 诱导用户 打开授权页面
+      await openSetting()
+      // 2.3 获取收货地址
+      const res3 = await chooseAddress();
+      console.log(res3);
+    }
+    // wx.getSetting({
+    //   success: (result1) => {
+    //     // 获取到了授权信息
+    //     console.log(result1);
+    //     const scopeAddress = result1.authSetting['scope.address'];
+    //     // 用户授权过  或者 用户 从来没有调用过收货地址  
+    //     if (scopeAddress === true || scopeAddress === undefined) {
+    //       // 1.1 调用收货地址
+    //       wx.chooseAddress({
+    //         success: (result2) => {
+    //           console.log(result2);
+    //         }
+    //       });
+    //     } else {
+    //       // 2.1用户 点击 拒绝收货地址  诱导用户 打开授权页面 再调用获取收货地址
+    //       wx.openSetting({
+    //         success: (result) => {
+    //           // 2.2 再去调用 获取收货地址
+    //           wx.chooseAddress({
+    //             success: (result3) => {
+    //               console.log(result3);
+    //             },
+    //             fail: () => {},
+    //             complete: () => {}
+    //           });
+    //         }
+    //       });
 
-        }
-      },
-      fail: () => {},
-      complete: () => {}
-    });
+    //     }
+    //   },
+    //   fail: () => {},
+    //   complete: () => {}
+    // });
 
   },
 
---
+++ utils/asyncWx.js
@@ -0,0 +1,46 @@
+/**
+ * promise的 wx.getSetting
+ */
+export const getSetting = () => {
+  return new Promise((reslove, reject) => {
+    wx.getSetting({
+      success: (result) => {
+        reslove(result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    });
+  })
+}
+/**
+ * promise的 wx.chooseAddress
+ */
+export const chooseAddress = () => {
+  return new Promise((reslove, reject) => {
+    wx.chooseAddress({
+      success: (result) => {
+        resolve(result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    });
+  })
+}
+
+/**
+ * promise的 wx.openSetting
+ */
+export const openSetting = () => {
+  return new Promise((resolve, reject) => {
+    wx.openSetting({
+      success: (result) => {
+        resolve(result)
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    });
+  })
+}
