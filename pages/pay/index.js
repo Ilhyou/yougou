f8ff95f758a38996e3fe9e594be685024fbb03b2
齐林河 <1277859500@qq.com>
2019-08-03 23:15:57 +0800

    支付页面-动态渲染

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,11 +1,19 @@
 // pages/pay/index.js
+/**
+ * 1 动态渲染的商品应该是 checked=true的这些商品
+ */
+import regeneratorRuntime from '../../lib/runtime/runtime';
+// import { getSetting, openSetting, chooseAddress, showModal } from "../../utils/asyncWx";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    address: {},
+    cart: {},
+    totalNum: 0,
+    totalPrice: 0
   },
 
   /**
@@ -25,8 +33,40 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
+  // 页面切换显示的时候 触发 onShow
   onShow: function () {
-
+    //  1 获取本地存储中的 收货地址数据  默认值 空字符串
+    const address = wx.getStorageSync("address") || {};
+    // 1 获取购物车数据
+    const cart = wx.getStorageSync("cart") || {};
+    // 2 把address存入到data中
+    this.setData({
+      address
+    });
+    this.setCart(cart);
+  },
+  setCart(cart) {
+    // 0 把购物车对象转成 数组 
+    let cartArr = Object.values(cart);
+    console.log(cartArr);
+    // 1 计算是否都选中了
+    // every 会接收一个回调函数 当所有循化项都返回 true的时候 cartArr.every的返回值 才会是true 
+    // every 当是空数组调用它的时候 返回值就是true
+    // 2 计算总价格 只计算了勾选的商品的价格 
+    let totalPrice = 0;
+    // 3 计算总数量 
+    let totalNum = 0;
+    cartArr.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num;
+      }
+    })
+    this.setData({
+      cart,
+      totalPrice,
+      totalNum
+    });
   },
 
   /**
